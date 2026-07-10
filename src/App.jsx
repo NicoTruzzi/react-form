3bbb84b0c7d68ad969713c46f24ddb8d2859d016
Nicolò <nicolo.truzzi1@gmail.com>
2026-07-10 17:53:50 +0200

    esercizio 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
 import { useState } from "react"
 
 function App() {
-  const [promoCode, setPromoCode] = useState("");
+  const [voto, setVoto] = useState("");
+  const [comment, setComment] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const [message, setMessage] = useState("");
   const handleSubmit = (event) => {
-    event.preventDefault();
-    if (promoCode === "SUMMER10") {
-      setMessage("codice valido")
+    event.preventDefault()
+    const votoHandle = Number(voto);
+    if (votoHandle === 0 || comment.trim() === "") {
+      setMessage("inserisci tutti i campi per inviare");
+
+      return;
     }
-    else
-      setMessage("codice errato riprova")
+    if (votoHandle < 4 && votoHandle > 0) {
+      setIsSubmitted(true);
+      setMessage("ci dispiace dobbiamo fare meglio")
+    } else {
+      setMessage("grazie per la recensione positiva")
+      setIsSubmitted(true);
+    }
   }
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      {
+        !isSubmitted &&
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={comment} onChange={event => (setComment(event.target.value))} />
+          <input type="radio" name="votes" value="1" onChange={event => (setVoto(event.target.value))} />
+          <input type="radio" name="votes" value="2" onChange={event => (setVoto(event.target.value))} />
+          <input type="radio" name="votes" value="3" onChange={event => (setVoto(event.target.value))} />
+          <input type="radio" name="votes" value="4" onChange={event => (setVoto(event.target.value))} />
+          <input type="radio" name="votes" value="5" onChange={event => (setVoto(event.target.value))} />
+        </form>
+      }
+      {message &&
+        <p>{message}</p>
+      }
 
-        <input type="text" placeholder="inserisci il codice promo" value={promoCode} onChange={(event) => (setPromoCode(event.target.value))} />
-
-      </form>
-      {message && <p>{message}</p>}
     </>
-    //il paragrafo viene creato solo se c'è qualcosa dentro message
   )
 }
 
